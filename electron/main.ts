eee0d965d19d9011413c404966922fbbca13a4ac
OwlVi <mrowlvi@gmail.com>
2026-04-02 15:12:27 +0700

    Import: Declare but never read

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, shell } from 'electron'
+import { app, BrowserWindow, ipcMain } from 'electron'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 
